2857a789435c1854f26f0262565d94666e183219
lettenj61 <lettenj61@users.noreply.github.com>
2018-12-22 15:54:12 +0900

    re-think polyfill

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,4 +1,3 @@
-import { Eyes } from './selector';
 import Hands from './event';
 
 ((window: any, document: any) => {
@@ -22,23 +21,55 @@ import Hands from './event';
     if (hands == null) {
       hands = new Hands();
     }
-    const createAddListener = (self: any) => {
-      return (type: string, callback: (e?: Event) => any) => {
-        hands!.addListener(self as Element, type, callback);
+
+    const inject = (self: any) => {
+      if (self !== undefined && self !== null) {
+        if (self.length) {
+          const len = self.length as number;
+          for (let i = 0; i < len; i++) {
+            inject(self[i]);
+          }
+        } else {
+          self.addEventListener =
+            self.addEventListener ||
+            function(type: string, callback: (e?: Event) => any): void {
+              hands!.addListener(self as Element, type, callback);
+            };
+  
+          self.removeEventListener =
+            self.removeEventListener ||
+            function(type: string, callback: (e?: Event) => any): void {
+              hands!.removeListener(self as Element, type, callback);
+            };
+        }
+      }
+      return self;
+    };
+
+    const hijack = (fn: string) => {
+      if (fn in document) {
+        const native = document[fn];
+        document[fn] = function(arg: any) {
+          return inject(native(arg));
+        };
       }
     };
+
+    inject([window, document]);
+
+    hands!.addListener(document as Element, 'readystatechange', (e?: Event) => {
+      inject(document.all);
+    });
+
+    hijack('getElementById');
+    hijack('getElementsByName');
+    hijack('getElementsByTagName');
+    hijack('createElement');
+
+    // for querySelector polyfill
+    hijack('querySelector');
+    hijack('querySelectorAll');
+
     window['__handsjs__polyfill'] = hands;
-    window.addEventListener = createAddListener(window);
-    document.addEventListener = createAddListener(document);
-    const prevFn = document.onreadystatechange;
-    document.onreadystatechange = function() {
-      for (let i = 0; i < document.all.length; i++) {
-        const el = document.all[i];
-        el.addEventListener = createAddListener(el);
-      }
-      if (typeof prevFn === 'function') {
-        prevFn();
-      }
-    }
   }
 })(window, document);
